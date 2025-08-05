d235e99d61c8b39407750ad2044eaccbe397ceb3
Pratyushanand26 <anandpratyush26@gmail.com>
2025-08-05 22:25:08 +0530

    writer done

--- worker/worker.go
+++ worker/worker.go
@@ -8,12 +8,14 @@ type RawJson struct {
 }
 
 func Newentry(in <-chan RawJson, out chan<- RawJson) {
-	
+
 	data := <-in
 	if data.score <= 50 {
 		data.status = "fail"
 	}else{
 		data.status="pass"
 	}
+
+	out<-data
 
 }
--- writer/writer.go
+++ writer/writer.go
@@ -1,1 +1,33 @@
 package writer
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type RawJson struct {
+	id     int
+	name   string
+	score  int
+	status string
+}
+
+func Write(in <-chan RawJson) {
+
+	dir := `D:\Projects\random\json-proc`
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		fmt.Println("error during making directory", err)
+	}
+
+	rawdata:=<-in
+
+	data,err:=json.Marshal(rawdata)
+	if err!=nil{
+		log.Fatal(err)
+	}
+
+	os.WriteFile(dir,data,0655);
+}
